2cdb37e604b80721d2590b7f6e439a63c178ddbf
Augusto Barbosa <augustocbarbosa@gmail.com>
2025-05-15 05:42:20 +0000

    Publish 4 from staging into origin/main

--- sections/SimpleFooter.tsx
+++ sections/SimpleFooter.tsx
@@ -15,18 +15,14 @@ export default function Footer({
   cta = [],
 }: Props) {
   return (
-    <footer class="footer footer-center p-10 bg-base-100 text-base-content">
-      <div class="grid grid-flow-col gap-4">
+    <footer class="footer footer-center center text-center grid gap-8 p-10 bg-base-100 text-base-content">
+      <div class="flex gap-2 center justify-center">
         {cta?.map((item) => (
           <a
             key={item?.id}
             id={item?.id}
             href={item?.href}
             target={item?.href.includes("http") ? "_blank" : "_self"}
-            class={`font-normal btn btn-primary
-              ${!item.style || item.style == "Ghost" && "btn-ghost"}
-              ${item.style == "Outline" && "btn-outline"}
-            `}
           >
             {item?.text}
           </a>
