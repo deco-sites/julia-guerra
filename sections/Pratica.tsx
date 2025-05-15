e28cb9941fc4b511caf12bb5fc463b4a943c3e5c
Augusto Barbosa <augustocbarbosa@gmail.com>
2025-05-15 18:03:59 +0000

    Publish 4 from staging into origin/main

--- sections/Pratica.tsx
+++ sections/Pratica.tsx
@@ -1,7 +1,13 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
-export interface CTA {
+export interface Testing {
+  label: string;
+  /** @format rich-text */
+  text: string;
+}
+
+export interface Paragraph {
   id?: string;
   href: string;
   label: string;
@@ -11,18 +17,12 @@ export interface CTA {
 export interface Props {
   placement?: "left" | "right" | "center";
   title?: string;
-  titlePlacement?: "left" | "right" | "center";
+  tagline?: string;
   /** @format rich-text */
   description?: string;
-  descriptionPlacement?: "left" | "right" | "center" | "justify";
-  tagline?: string;
+  paragraphs: Paragraph[];
   image?: ImageWidget;
   cta?: CTA[];
-  disableSpacing?: {
-    top?: boolean;
-    bottom?: boolean;
-  };
-  ctaPlacement?: "left" | "right" | "center";
 }
 
 const PLACEMENT = {
@@ -31,39 +31,22 @@ const PLACEMENT = {
   center: "flex-row justify-center",
 };
 
-const CTA_PLACEMENT = {
-  left: "start",
-  right: "end",
-  center: "center",
-};
-
-const DEFAULT_IMAGE =
-  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4763/772e246e-1959-46ac-a309-3f25ab20af6f";
-
 export default function ImageWithParagraph({
-  title = "",
-  titlePlacement = "left",
+  title = "Title",
   description =
     "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
-  descriptionPlacement = "left",
   tagline = null,
   image = null,
   placement = "left",
-  disableSpacing,
-  cta = [
-    { id: "change-me-1", href: "/", label: "Change me", style: "Outline" },
-    { id: "change-me-2", href: "/", label: "Change me", style: "Ghost" },
-  ],
-  ctaPlacement = "left",
+  paragraphs = [],
+  cta = [],
 }: Props) {
   return (
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4">
       <div
         class={`flex ${
           PLACEMENT[placement]
-        } gap-12 md:gap-20 text-left items-center z-10 ${
-          disableSpacing?.top ? "" : "pt-12 lg:pt-28"
-        } ${disableSpacing?.bottom ? "" : "pb-12 lg:pb-28"}`}
+        } gap-12 md:gap-20 text-left items-center z-10`}
       >
         
           { image && <div class="w-full md:w-1/2 border border-secondary rounded-lg overflow-hidden">
@@ -79,7 +62,7 @@ export default function ImageWithParagraph({
             />
         </div> }
         <div class="w-full gap-4 z-10 md:max-w-xl md:w-1/2 md:space-y-4 space-y-2">
-          <p class="text-4xl leading-snug" style={`text-align:${titlePlacement}`}>
+          <p class="text-3xl leading-snug">
             {title}
           </p>
           {tagline ? <p class="text-sm text-secondary">
@@ -87,13 +70,13 @@ export default function ImageWithParagraph({
           </p>
           : ''}
           <div
-            class="grid gap-4 leading-[1.6]" style={`text-align:${descriptionPlacement}`}
+            class="grid gap-4 leading-[1.6]"
             dangerouslySetInnerHTML={{
               __html: description,
             }}
           >
           </div>
-          <div class={`flex gap-3 pt-4 justify-${CTA_PLACEMENT[ctaPlacement]}`}>
+          <div class={`flex gap-3 pt-4`}>
             {cta?.map((item) => (
               <a
                 key={item?.id}
